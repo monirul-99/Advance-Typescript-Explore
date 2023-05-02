5e20490121ac85d179a65f2e90ca9b16d7a88b12
monaulic <sohelmuhammad164@gmail.com>
2023-05-02 23:24:37 +0600

    Alias and Inerface extra learn

--- src/interface.ts
+++ src/interface.ts
@@ -4,7 +4,7 @@ type User = {
     age : number
 }
 
-
+//this is made by union & intersection
 type extendUser = User &  {
     role : string
 }
@@ -19,6 +19,32 @@ interface IUser{
 interface IExtendedUser extends IUser {
     role : string
 }
+
+//object function array --> special convenience type of object
+//Alias Type Process
+type addNumberType = (num1: number, num2: number) => number
+
+//Interface Type Process
+interface IAddNumberType {
+    (num1: number, num2: number) : number
+}
+//call with union type 
+//Better Advice: You can use union type for Func bcz union type is more readable, clean compare to interface type 
+const addNumbersUnion: addNumberType = (num1, num2) => num1 + num2
+//call with Interface type
+const addNumbersInterface: IAddNumberType = (num1, num2) => num1 + num2
+
+//Alias Type
+type rollNumbersType = number[]
+//Interface Type
+interface IRollNumberType {
+    [index: number] : number
+}
+//build to Alias
+const rollNumbersAlias: rollNumbersType = [1, 2, 4]
+//Build to interface
+const rollNumbers: IRollNumberType = [1, 2, 4]
+
 
 const userWithTypeAlias: extendUser = {
     name: "Type Alias",
