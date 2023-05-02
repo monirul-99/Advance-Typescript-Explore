7844231f9a39a8686389f624153024076915ab5a
monaulic <sohelmuhammad164@gmail.com>
2023-05-03 00:18:42 +0600

    genaric type explained

---
+++ src/generic-type.ts
@@ -0,0 +1,26 @@
+type GenericArray<T> = Array<T>
+
+//Something Look Like Functions Parameters
+const rollNumbersAsNumber:GenericArray<number> = [1,3,4]
+const rollNumbersAsString: GenericArray<string> = ["Nothing","Something","Anything"]
+const rollNumbersAsBoolean:GenericArray<boolean> = [true, false]
+
+
+type NameRollType = {name: string, roll: number}
+
+const userNameAndRollNumbers: GenericArray<NameRollType> = [
+    {name: "Monirul", roll: 2},
+    {name: "Sadia", roll: 1},
+]
+
+type GenericTuple<X, Y> = [X,Y]
+type RelationType = {
+    name: string, salary: number
+}
+
+const relation:GenericTuple<string, string> = ["Persian", "kate Ria"]
+//Not Good Practice BCZ object inside any data provide
+const nearByRelation:GenericTuple<RelationType, string> = [{name: "Monirul", salary: 25000}, "Front-End Developer"]
+
+//Good practice
+const goodRelation:GenericTuple<RelationType, string> = [{name: "Monirul", salary: 25000}, "Front-End Developer"]
